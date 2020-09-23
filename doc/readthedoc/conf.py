ff1b4f30fb8d1e42ea15a336e6881cf888ca4a3c
weiwee <wbwmat@gmail.com>
2020-09-23 14:04:07 +0800

    rename rethedocs
    Signed-off-by: weiwee <wbwmat@gmail.com>

--- doc/readthedoc/conf.py
+++ doc/readthedoc/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -39,7 +39,7 @@ autosummary_generate = True
 source_suffix = ['.rst']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['doc/readthedoc/_templates']
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -56,7 +56,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['doc/readthedoc/_static']
+html_static_path = ['_static']
 html_context = {
     'css_files': [
         '_static/theme_overrides.css',  # override wide tables in RTD theme
